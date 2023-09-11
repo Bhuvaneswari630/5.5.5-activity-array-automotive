1ab698615dc66619ac3fcc70a85630006615d147
Bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2023-09-11 13:25:09 -0400

    completed assignment

--- factory.js
+++ factory.js
@@ -1,3 +1,22 @@
-class Factory{
-    
+class Factory {
+    constructor(location, owner) {
+        this.location = location
+        this.owner = owner
+        this.vehicles = []
+    }
+
+    listVehicles() {
+        return this.vehicles
+    }
+    createVehicle(vehicle) {
+        this.vehicles.push(vehicle);
+        // console.log(this.vehicles);
+    }
+    paintVehicle(vin, paintColor) {
+        this.vehicles.find(function (v) {
+            if (v.vin === vin) {
+                v.paint(paintColor)
+            }
+        })
+    }
 }
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,20 @@
 class Vehicle {
-    
+    constructor(make, model, year, price, vin, color){
+        this.make = make
+        this.model = model
+        this.year = year
+        this.color = color
+        this.price = price
+        this.vin = vin
+    }
+    drive(){
+        // console.log (this.year + " " + this.make + " " + this.model + ' drives');
+        return (this.year + " " + this.make + " " + this.model + ' drives');
+    }
+    paint(color){
+        return this.color = color
+        // console.log(`${this.make} painted ${color}`);
+    }
 }
+
 
